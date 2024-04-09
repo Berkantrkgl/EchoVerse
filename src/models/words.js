9de648b7a51830bc715b5714f565a065fbb48e99
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2024-04-09 19:39:25 +0300

    Edit words models for development stage

--- src/models/words.js
+++ src/models/words.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema;
 var wordSchema = new Schema({
     en_word: { type: String, required: true, lowercase: true}, 
     tr_word: { type: String, required: true, lowercase: true},
-    creator: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }], 
+    //creator: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }], 
     entry_date: { type: Date, required: true, default: Date.now },
+    is_active: { type: Boolean, required: true, default: true},
     number_of_seen: { type: Number,  required: true, default: 1}
 });
 
