9f199f1776823305baf6c04f1c4a1af832948ea4
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2024-05-04 20:11:33 +0300

    Try to configure flash middleware

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const appRouter = require("./src/routes/appRoutes");
 const userRouter = require("./src/routes/userRoutes");
 const models = require("./src/models");
 const methodOverride = require("method-override");
+
+// Send messages to user
+const session = require("express-session");
+const flash = require("connect-flash");
+
+const sessionOptions = {
+    secret: "thisisnotgoodsecret",
+    resave: false,
+    saveUninitialized: false,
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+// Port number
 const port = 3000;
 
 // Database connection
@@ -20,6 +35,12 @@ app.use(methodOverride("__method"));
 // Routing
 app.use("/", appRouter);
 app.use("/user", userRouter);
+
+// Flash middleware
+app.use((req, res, next) => {
+    res.locals.message = req.flash("success");
+    next();
+});
 
 // Running the server
 app.listen(port, () => {
