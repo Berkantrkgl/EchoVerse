43ed3f582dd56a2f829f59250c8efee7143d7563
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2024-05-08 14:14:14 +0300

    Add user with session to words page

--- src/controllers/wordController.js
+++ src/controllers/wordController.js
@@ -1,6 +1,8 @@
 const Word = require("../models/words");
+const User = require("../models/users");
 const asyncHandler = require("express-async-handler");
 
 exports.get_all_learned_words = asyncHandler(async (req, res, next) => {
-    res.render("words.ejs");
+    const user = await User.findById(req.session.user_id);
+    res.render("words.ejs", { user: user });
 });
