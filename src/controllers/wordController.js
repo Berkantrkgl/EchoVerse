2123ef99af8aa39e10e82cae17c9bcdf4a929f8c
Berkan Turkoglu <e190501030@stud.tau.edu.tr>
2024-05-08 15:10:07 +0300

    Add session to words page

--- src/controllers/wordController.js
+++ src/controllers/wordController.js
@@ -3,6 +3,7 @@ const User = require("../models/users");
 const asyncHandler = require("express-async-handler");
 
 exports.get_all_learned_words = asyncHandler(async (req, res, next) => {
+    console.log(req.session);
     const user = await User.findById(req.session.user_id);
     res.render("words.ejs", { user: user });
 });
